3f000c95e0248841e637c7f0a265b3e9334ba6e3
Cezar Sîrbu <sirbunicolaecezar@gmail.com>
2018-09-20 17:47:03 +0300

    Create Application.java

---
+++ src/main/java/com/pidgeon/todos/Application.java
@@ -0,0 +1,11 @@
+package com.pidgeon.todos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockmanagerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StockmanagerApplication.class, args);
+	}
+}
