88fc04e59f6a9e095448da69b4bb88ec12da325d
Cezar Sîrbu <sirbunicolaecezar@gmail.com>
2018-09-20 17:49:16 +0300

    Create Todo.java

---
+++ src/main/java/com/pidgeon/todos/models/Todo.java
@@ -0,0 +1,28 @@
+package com.pidgeon.todos.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+  @Id
+  @GeneratedValue(strategy= GenerationType.AUTO)
+  private long id;
+  private String task;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getTask() {
+    return task;
+  }
+
+  public void setTask(String task) {
+    this.task = task;
+  }
+}
