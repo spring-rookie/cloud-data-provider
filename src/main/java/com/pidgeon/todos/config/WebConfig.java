ab2b8c8849a528257312d1954747f6064696ce2f
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2018-09-21 01:09:12 +0300

    Add tests and config package

---
+++ src/main/java/com/pidgeon/todos/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.pidgeon.todos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig {
+  @Bean
+  public Filter shallowEtagHeaderFilter() {
+    return new ShallowEtagHeaderFilter();
+  }
+}
