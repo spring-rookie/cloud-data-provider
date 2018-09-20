057d7f33a28401717959a2f296ad6540f0c617a4
Cezar Sîrbu <sirbunicolaecezar@gmail.com>
2018-09-20 17:51:59 +0300

    Update Application.java

--- src/main/java/com/pidgeon/todos/Application.java
+++ src/main/java/com/pidgeon/todos/Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StockmanagerApplication {
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(StockmanagerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
