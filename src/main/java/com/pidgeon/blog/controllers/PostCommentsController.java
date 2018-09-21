b30449319f58e2ca120c53733ed16689931aa96e
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2018-09-22 00:18:01 +0300

    Add get post comments endpoint

--- src/main/java/com/pidgeon/blog/controllers/PostCommentsController.java
+++ src/main/java/com/pidgeon/blog/controllers/PostCommentsController.java
@@ -4,6 +4,9 @@ import com.pidgeon.blog.models.Comment;
 import com.pidgeon.blog.models.Post;
 import com.pidgeon.blog.services.CommentService;
 import com.pidgeon.blog.services.PostService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -26,5 +29,12 @@ public class PostCommentsController {
     Comment savedC = commentService.save(c);
 
     return savedC;
+  }
+
+  @RequestMapping(value = "", method = RequestMethod.GET)
+  public ResponseEntity<Iterable<Comment>> comments(@PathVariable Long postId) {
+    Optional<Post> p = postService.findById(postId);
+
+    return new ResponseEntity<>(p.get().getComments(), new HttpHeaders(), HttpStatus.OK);
   }
 }
