5449e662784818f8cf1102a1ba1d0fa3543f162d
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2018-10-16 00:41:32 +0300

    Remove already existing methods

--- src/main/java/com/pidgeon/blog/repositories/CommentRepository.java
+++ src/main/java/com/pidgeon/blog/repositories/CommentRepository.java
@@ -3,8 +3,5 @@ package com.pidgeon.blog.repositories;
 import com.pidgeon.blog.models.Comment;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface CommentRepository extends CrudRepository<Comment, Long> {
-  Optional<Comment> findById(Long id);
 }
--- src/main/java/com/pidgeon/blog/repositories/PostRepository.java
+++ src/main/java/com/pidgeon/blog/repositories/PostRepository.java
@@ -3,8 +3,5 @@ package com.pidgeon.blog.repositories;
 import com.pidgeon.blog.models.Post;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface PostRepository extends CrudRepository<Post, Long> {
-  Optional<Post> findById(Long id);
 }
